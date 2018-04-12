1e9c5c77adc8cb00ea086126f10b77ec9e329227
SEAN <cdnumber1@hotmail.com>
2018-04-12 17:58:12 +1000

    This is commiting the overhaul to the new SwinGame version

--- BattleShips C#/src/GameMain.cs
+++ BattleShips C#/src/GameMain.cs
@@ -1,5 +1,6 @@
 using System;
 using SwinGameSDK;
+using static SwinGameSDK.SwinGame;
 //using static SwinGameSDK.SwinGame; // requires mcs version 4+,
 using BattleShip;
 
@@ -14,8 +15,9 @@ namespace MyGame
         public static void Main()
         {
             SwinGame.OpenGraphicsWindow("BattleShips", 800, 600);
+           // ShowSwinGameSplashScreen(); //This wasn't in the vB
             //Load Resources
-            
+
             GameResources res = new GameResources();
             GameController con = new GameController(res);
             con._resources.LoadResources();
@@ -23,35 +25,32 @@ namespace MyGame
 
             SwinGame.PlayMusic(con._resources.GameMusic("Background"));
 
-            //Game Loop
-            do
-            {
-                con.HandleUserInput();
-                con.DrawScreen();
-            } while (!(SwinGame.WindowCloseRequested() == true | con.CurrentState == GameState.Quitting));
+            /* //Game Loop
+             do
+             {
+                 con.HandleUserInput();
+                 con.DrawScreen();
+             } while (!(SwinGame.WindowCloseRequested() == true | con.CurrentState == GameState.Quitting));
+             */
 
-            SwinGame.StopMusic();
+            ////Run the game loop
+            while (false == SwinGame.WindowCloseRequested() && con.CurrentState != GameState.Quitting)
+            {
+                //Fetch the next batch of UI interaction
+                con.HandleUserInput();
+
+                //Clear the screen and draw the framerate
+                ClearScreen(Color.White);
+                DrawFramerate(0, 0);
+
+                //Draw onto the screen
+                con.DrawScreen();
+            }
+
+            StopMusic();
 
             //Free Resources and Close Audio, to end the program.
             con._resources.FreeResources();
-
-            ////Open the game window
-            //OpenGraphicsWindow("GameMain", 800, 600);
-            //ShowSwinGameSplashScreen();
-            
-            ////Run the game loop
-            //while(false == WindowCloseRequested())
-            //{
-            //    //Fetch the next batch of UI interaction
-            //    ProcessEvents();
-                
-            //    //Clear the screen and draw the framerate
-            //    ClearScreen(Color.White);
-            //    DrawFramerate(0,0);
-                
-            //    //Draw onto the screen
-            //    RefreshScreen(60);
-            //}
         }
     }
 }
